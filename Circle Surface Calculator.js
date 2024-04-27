31e19f457dd6fe8f9c5a19c202292cfcd2e496c2
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-27 14:57:16 +0100

    Circle Surface Calculator.js

---
+++ Circle Surface Calculator.js
@@ -0,0 +1,6 @@
+function surface()
+    {
+        var r = parseFloat(prompt("saisir un Rayon"));
+        return("la surface de cercle correspondant est : " + 3.14*r*r )
+    }
+    console.log("la surface de cercle correspondant est : ",Surface())
